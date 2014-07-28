0f5eafd3d20fbe6a8aeef8852811791f183171cf
robert-vo <robertvo79@gmail.com>
2014-07-28 10:37:23 -0500

    7/28/2014 - Committing before I insert the images in from Sasha.

--- app/js/app.js
+++ app/js/app.js
@@ -1,39 +1,59 @@
 'use strict';
-
 /* App Module */
-
 var farmersMarketApp = angular.module('farmersMarketApp', [
   'ngRoute',
-  'farmersMarketControllers'
+  'farmersMarketControllers',
+  'ngAnimate',
+  'ui.router',
+  'AngularGM'
 ]);
 
+//URI whitelist implementation
+farmersMarketApp.config(function($compileProvider){
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|comgooglemaps|geo|tel):/);
+})
 
-farmersMarketApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/farmers', {
-        templateUrl: 'partials/farmersMain.html',
-        controller: 'FarmersListCtrl'
-      }).
-      when('/farmersDirectory', {
-        templateUrl:'partials/farmersDirectory.html',
-        controller: 'FarmersDetailCtrl'
-      }).
-	  when('/farmersFavorites', {
-        templateUrl:'partials/farmersFavorites.html',
-        controller: 'FarmersDetailCtrl'
-      }).
-	  when('/farmersMore', {
-        		templateUrl:'partials/farmersMore.html',
-        controller: 'FarmersDetailCtrl'
-      }).
-	
-      otherwise({
-        redirectTo: '/farmers'
-      });
-  }]);
-
-function farmersMarketControllers($scope){
-	$scope.visible=true;
-	
-}
+//state provider
+farmersMarketApp.config(function($stateProvider, $urlRouterProvider){
+      $urlRouterProvider.otherwise("/")
+      $stateProvider
+            .state('markets', {
+            url: "/markets",
+            templateUrl: "partials/markets.html",
+			controller: 'FarmersMapCtrl'
+        })
+            .state('markets.info', {
+              url: "/:id",
+              templateUrl: "partials/markets.info.html",
+              controller : "FarmersMapDetailCtrl"
+          })
+            .state('inSeason', {
+              url: "/inSeason",
+              templateUrl: "partials/inSeason.html",
+              controller : 'FarmersSeasonCtrl'
+          })
+            .state('qrScanner', {
+              url: "/qrScanner",
+              templateUrl: "partials/qrScanner.html",
+              controller : 'QRScannerCtrl'
+          })
+            .state('about', {
+              url: "/about",
+              templateUrl: "partials/about.html"
+          })
+            .state('events', {
+              url: "/events",
+              templateUrl : "partials/events.html",
+              controller : 'EventsCtrl'
+      })
+            .state('events.info', {
+              url:"/:id",
+              templateUrl : "partials/events.info.html",
+              controller : 'EventsDetailCtrl'   
+      })
+            .state('test', {
+            url:"/test",
+            templateUrl : "partials/test.html",
+            controller : 'TestCtrl'    
+      })
+})
